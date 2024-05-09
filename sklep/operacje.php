22e77936cc7eb766d03c13f2080602346f363519
Artur Pelo <pelo.artur@gmail.com>
2024-05-09 09:33:26 +0200

    Dodano możliwość sprzedaży towaru

--- sklep/operacje.php
+++ sklep/operacje.php
@@ -1,1 +1,49 @@
-operacje
+<h1> Operacje</h1>
+
+<form method="POST" action="zakup.php">
+<input type="date" name="f_data">
+<br>Klient:
+<select name="f_klient">
+
+    
+
+    <?PHP
+    include 'dbconfig.php';
+    session_start();
+    $baza = mysqli_connect($server,$user,$pass,$base) or ('cos nie tak z polaczenie z BD');
+
+    $zapytanie="SELECT id, nazwa FROM klienci ORDER BY nazwa ASC";
+    $result = $baza->query($zapytanie) or die ('bledne zapytanie');
+
+
+    while($wiersz = $result->fetch_assoc()){
+
+       echo "<option value='".$wiersz['id']."'>".$wiersz['nazwa']."</option>\n";
+    };
+
+
+?>
+  
+</select>
+<br>Towar:
+<select name="f_towar">
+    
+<?PHP
+    
+    $zapytanie="SELECT id, nazwa FROM towary ORDER BY nazwa ASC";
+    $result = $baza->query($zapytanie) or die ('bledne zapytanie');
+
+
+    while($wiersz = $result->fetch_assoc()){
+
+       echo "<option value='".$wiersz['id']."'>".$wiersz['nazwa']."</option>\n";
+    };
+
+    $baza->close();
+?>
+  
+</select>
+
+<br>
+<button type="submit"> Zapłacono </button> 
+</form>
---
+++ sklep/zakup.php
@@ -0,0 +1,21 @@
+<?php
+
+$klient=$_POST['f_klient'];
+$towar=$_POST['f_towar'];
+//$data=$POST['f_data'];
+$data=date("Y-m-d");
+
+    include 'dbconfig.php';
+    session_start();
+    $baza = mysqli_connect($server,$user,$pass,$base) or ('cos nie tak z polaczenie z BD');
+
+    $zapytanie="INSERT INTO `operacje` (id,idk,idt,data) VALUES (NULL, $klient,$towar,$data)";
+    $result = $baza->query($zapytanie) or die ('bledne zapytanie');
+
+    baza->close();
+
+?>
+
+
+
+?>
